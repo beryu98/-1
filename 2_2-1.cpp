1f8f38c1294d0523712785397e357a2d79c8f09c
beryu98 <wldnrghkd@gmail.com>
2020-10-01 13:04:33 +0900

    자료구조2주차2-1

---
+++ 2_2-1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//2017111866 황지욱
+//본인은 이 소스파일을 다른 사람의 소스를 복사하지 않고 직접 작성하였습니다.
+
+#define MALLOCI(p,s) \
+	if(!((p) = (int*) malloc(s))) {\
+		fprintf(stderr, "insufficient memory");\
+		exit (EXIT_FAILURE);\
+	}
+
+void func(int num) {
+	int* pNum = (int*)malloc(sizeof(int));
+	MALLOCI(pNum, sizeof(int));
+	*pNum = num;
+	printf("%d\n", *pNum);
+	free(pNum);
+}
+
+int main() {
+	int num = 1;
+	func(num);
+	printf("%d\n", num);
+
+	return 0;
+}
